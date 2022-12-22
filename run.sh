77a82be721034aa386c1718110c34cdbfd3820b8
Duy Nguyen <2051067158@e.tlu.edu.vn>
2022-12-22 09:15:34 -0800

    chinh lai tep bien dich cho Linux

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # sudo apt-get install freeglut3-dev
 # sudo apt-get install mesa-utils
 
-g++ main.cpp Imageloader.cpp Planet.cpp ScreenWriter.cpp SolarSystem.cpp -lglut -lGL -lGLU -lglfw ./lib/ikpFlac.so ./lib/ikpMP3.so ./lib/libIrrKlang.so  -I./include/ -o main &&
+g++ main.cpp Imageloader.cpp Planet.cpp ScreenWriter.cpp SolarSystem.cpp -lglut -lGL -lGLU -lglfw ./ikpFlac.so ./ikpMP3.so ./libIrrKlang.so  -I./include/ -o main &&
 
 ./main;
 
